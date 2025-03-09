e5ca3a10bac9a7ab722c0d334680e535a401209f
25-do <erickimosavej@gmail.com>
2025-03-09 20:43:06 +0300

    index for loop

--- shop/views.py
+++ shop/views.py
@@ -4,9 +4,13 @@ from .models import Product
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        products = Product.objects.all()[:3]
+        products = Product.objects.all()[:10]
+        products1 = Product.objects.all()[10:20]
+        products2 = Product.objects.all()[20:30]
 
         context = {
-            "products" : products
+            "products" : products,
+            "products1" : products1,
+            "products2" : products2
         }
         return render(request, "index.html", context)
